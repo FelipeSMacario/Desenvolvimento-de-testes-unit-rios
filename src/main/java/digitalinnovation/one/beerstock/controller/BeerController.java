532683556b121dcbb804c14b72452dacef534ad8
Felipe Macário <felipesmacario@gmail.com>
2021-06-19 21:48:36 -0300

    Parte2

---
+++ src/main/java/digitalinnovation/one/beerstock/controller/BeerController.java
@@ -0,0 +1,42 @@
+package digitalinnovation.one.beerstock.controller;
+
+import digitalinnovation.one.beerstock.service.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController implements BeerControllerDocs {
+
+    private final BeerService beerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyExistsException{
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listBeers(){
+        return beerService.listAll();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException{
+        beerService.deleteByID(id);
+    }
+
+    
+
+}
